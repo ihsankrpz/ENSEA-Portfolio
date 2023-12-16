d58325eb3d9ca8b4b66a211d5fc4015450f0e4ae
lostmart <martinpedraza1979@gmail.com>
2023-12-16 12:36:34 +0100

    new styles and stuff

---
+++ js/projects.js
@@ -0,0 +1,42 @@
+const projects = [
+	{
+		title: "Python Flask",
+		description: "lorem ipsum numero uno",
+		imageUrl: "flask-api.png",
+		technologies: ["Python", "Restful API"],
+		links: [
+			"https://replit.com/@MartinPedraza/basic-rest-flask#main.py",
+			"https://basic-rest-flask.martinpedraza.repl.co/",
+		],
+	},
+	{
+		title: "Landing Page",
+		description: "lorem ipsum numero dos",
+		imageUrl: "front-01.png",
+		technologies: ["Html", "Css"],
+		links: [
+			"https://github.com/web-rest-api/tp1-site",
+			"https://web-rest-api.github.io/tp1-site/",
+		],
+	},
+	{
+		title: "Game Manager",
+		description: "lorem ipsum numero tres",
+		imageUrl: "game-manager.png",
+		technologies: ["Bootstrap 5", "Javascript"],
+		links: [
+			"https://replit.com/@MartinPedraza/basic-rest-flask#main.py",
+			"https://basic-rest-flask.martinpedraza.repl.co/",
+		],
+	},
+	{
+		title: "Users",
+		description: "Uthentication API",
+		imageUrl: "nodeJs-express.png",
+		technologies: ["NodeJs", "Express"],
+		links: [
+			"https://replit.com/@MartinPedraza/node-expess#index.js",
+			"https://node-expess.martinpedraza.repl.co/",
+		],
+	},
+]
